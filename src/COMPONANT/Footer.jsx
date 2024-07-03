a0d2de1b36d07be9a93654e18e4e494c06dca80f
salman fursi <salmanfursipro@gmail.com>
2024-07-03 08:09:46 -0700

    footer

--- src/COMPONANT/Footer.jsx
+++ src/COMPONANT/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-gray-800 text-gray-300 py-4 mt-4">
       <div className="container mx-auto flex flex-col items-center">
       <div className="flex justify-center py-2">
-        <img src="/public/asikbhai-removebg-preview.png" alt="Your Logo" className="w-14 rounded-full border-white border-2" />
+        <img src="https://i.ibb.co/pRF10Rj/asikbhai-removebg-preview.png" alt="Your Logo" className="w-14 rounded-full border-white border-2" />
       </div>
         <div className="text-sm mb-1">
           saidurrahmanasik@gmail.com
